8bab03d3622ead5798e882b218a95b6135b4ac2b
Karolina Nowak <nowak.karolinna@gmail.com>
2020-06-24 21:30:16 +0200

    Variable

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myDepartment = "Chemistry";
+        int workExpirience = 4;
+        char drivingLicense = 'b';
+        boolean isCatLover = true;
+        double height = 1.6;
+        String favouriteBook = "Inferno";
+        System.out.println(myDepartment);
+        System.out.println(workExpirience);
+        System.out.println(drivingLicense);
+        System.out.println(isCatLover);
+        System.out.println(height);
+        System.out.println(favouriteBook);
+
+    }
+}
