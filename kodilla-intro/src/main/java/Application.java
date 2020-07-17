8c273f2422a3444e4e768321a825f8387c43f28a
Karolina Nowak <nowak.karolinna@gmail.com>
2020-07-17 20:38:56 +0200

    Submodule 2.6

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,41 @@
+public class Application {
+    public static void main(String[] args) {
+        Person person = new Person("Adam", 40.5, 178);
+        person.checkAge();
+    }
+}
+
+class Person {
+
+    private String name;
+    private double age;
+    private double height;
+
+    public Person(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void checkAge(){
+        if(this.name != null){
+            if(this.getAge() > 30 && this.getHeight() > 160){
+                System.out.println("User is older than 30 and higher then 160cm");
+            } else {
+                System.out.println("User is younger than 30 or lower than 160cm");
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
