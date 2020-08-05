000d3f78e8fda3d3a96d9b1e2b0bcd8a6be24a3e
Karolina Nowak <nowak.karolinna@gmail.com>
2020-08-05 19:22:34 +0200

    Module 5.3

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/ImmutableExample.java
@@ -0,0 +1,14 @@
+package com.kodilla.collections.adv.immutable;
+
+import java.util.Arrays;
+
+public class ImmutableExample {
+
+    public static void main(String[] args) {
+        Book book = new BookHacked("John Stewart", "The last chance");
+        BookHacked bookHacked = (BookHacked) book;
+        bookHacked.modifyTitle("New Title");
+        System.out.println(book.getTitle());
+
+    }
+}
