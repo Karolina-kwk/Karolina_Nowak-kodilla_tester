9a8d05c8e5b113cea779d92053e64eec0bfe44c1
Karolina Nowak <nowak.karolinna@gmail.com>
2020-10-24 19:19:28 +0200

    Module 14.3

--- kodilla-cucumber/src/test/java/numberchecker/IsItDivisibleNumberSteps.java
+++ kodilla-cucumber/src/test/java/numberchecker/IsItDivisibleNumberSteps.java
@@ -10,47 +10,39 @@ public class IsItDivisibleNumberSteps implements En {
 
     public IsItDivisibleNumberSteps() {
         Given("checking number is 2", () -> {
-            // Write code here that turns the phrase above into concrete actions
             this.number = 2;
         });
 
         Given("checking number is 5", () -> {
-            // Write code here that turns the phrase above into concrete actions
             this.number = 5;
         });
 
         Given("checking number is 9", () -> {
-            // Write code here that turns the phrase above into concrete actions
             this.number = 9;
         });
 
         Given("checking number is 15", () -> {
-            // Write code here that turns the phrase above into concrete actions
             this.number = 15;
         });
 
         Given("checking number is 21", () -> {
-            // Write code here that turns the phrase above into concrete actions
             this.number = 21;
         });
 
         Given("checking number is 30", () -> {
-            // Write code here that turns the phrase above into concrete actions
             this.number = 30;
         });
 
         Given("checking number is 40", () -> {
-            // Write code here that turns the phrase above into concrete actions
             this.number = 40;
         });
 
         Given("checking number is 45", () -> {
-            // Write code here that turns the phrase above into concrete actions
+
             this.number = 45;
         });
 
         Given("checking number is 90", () -> {
-            // Write code here that turns the phrase above into concrete actions
             this.number = 90;
         });
 
